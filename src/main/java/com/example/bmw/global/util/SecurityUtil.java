3a1104cf28974c9b6a78670e3765712e32bbef10
wheeeung <whee050916@gmail.com>
2022-10-27 11:56:01 +0900

    ♻️ :: securityUtil

--- src/main/java/com/example/bmw/global/util/SecurityUtil.java
+++ src/main/java/com/example/bmw/global/util/SecurityUtil.java
@@ -1,12 +1,20 @@
 package com.example.bmw.global.util;
 
+import com.example.bmw.domain.user.controller.dto.UserDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 
+@Slf4j
+@Component
 public class SecurityUtil {
     public static String getLoginUserEmail(){
-        Authentication loggedInUser = SecurityContextHolder.getContext().getAuthentication();
-        return loggedInUser.getName();
+        log.info(SecurityContextHolder.getContext().getAuthentication().getName());
+        Object user = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UserDto userDto = (UserDto)user;
+        log.info(userDto.getEmail());
+        return userDto.getEmail();
     }
 }
