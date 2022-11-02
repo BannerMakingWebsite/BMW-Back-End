6298908bc3852a65693cfb2433e7e6bb39a6c52d
wheeeung <whee050916@gmail.com>
2022-11-02 11:43:36 +0900

    📑 :: edit design

--- src/main/java/com/example/bmw/domain/design/controller/DesignController.java
+++ src/main/java/com/example/bmw/domain/design/controller/DesignController.java
@@ -1,11 +1,11 @@
 package com.example.bmw.domain.design.controller;
 
 import com.example.bmw.domain.design.controller.dto.reqeust.DesignNameRequest;
+import com.example.bmw.domain.design.controller.dto.reqeust.DesignRequest;
 import com.example.bmw.domain.design.entity.Design;
 import com.example.bmw.domain.design.service.DesignService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
@@ -15,7 +15,7 @@ public class DesignController {
     private final DesignService designService;
 
     @PostMapping("/design")
-    public Design save(@ModelAttribute DesignNameRequest request) throws IOException {
+    public Design save(@ModelAttribute DesignRequest request) throws IOException {
         return designService.save(request.getFile(), request.getDesignName());
     }
 
